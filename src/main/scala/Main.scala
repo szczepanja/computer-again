796e5a1d1758c522040d68fabd844b559a38c885
a_szczepanik <a.szczepanik@adform.com>
2022-03-30 20:18:14 +0200

    create case clas for sum/multiply/minus/divide

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,22 +1,47 @@
 import scala.io.Source
 
-case class Sum(symbol: Char, x: Int)
+case class State(x: Int) {}
 
-case class Minus(x: Int)
+case class Sum(symbol: Char, x: Int) {}
 
-case class Multiply(x: Int)
+case class Minus(symbol: Char, x: Int)
 
-case class Divide(x: Int)
+case class Multiply(symbol: Char, x: Int)
+
+case class Divide(symbol: Char, x: Int)
 
 object Main extends App {
   val fileName = "instructions.txt"
   val lines = Source.fromFile(fileName).getLines().toSeq
 
-  lines.map {
+  val result = "result"
+
+  val instructions = lines.map {
+    case line if line.startsWith("0") => {
+      val Array(x) = line.split("\\s+")
+      State(x.toInt)
+    }
     case line if line.startsWith("+") => {
       val Array(symbol, x) = line.split("\\s+")
       Sum(symbol.charAt(0), x.toInt)
     }
+    case line if line.startsWith("-") => {
+      val Array(symbol, x) = line.split("\\s+")
+      Minus(symbol.charAt(0), x.toInt)
+    }
+    case line if line.startsWith("*") => {
+      val Array(symbol, x) = line.split("\\s+")
+      Multiply(symbol.charAt(0), x.toInt)
+    }
+    case line if line.startsWith("/") => {
+      val Array(symbol, x) = line.split("\\s+")
+      Divide(symbol.charAt(0), x.toInt)
+    }
     case _ => true
-  }.foreach(println)
+  }
+
+  instructions.foldLeft(0) { (s, cmd) =>
+    s.sum()
+    s.execute(cmd)
+  }
 }
